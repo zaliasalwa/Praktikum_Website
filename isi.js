582b67b98f43d3d3d120c48ffc966c40e4af0542
Zalia Salwa <166020606+zaliasalwa@users.noreply.github.com>
2024-10-02 14:01:36 +0700

    isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("<h1> UNIVERSITAS LAMPUNG </h1>");
+document.writeln("<h2> PENDIDIKAN TEKNOLOGI INFORMASI </h2>");
+document.writeln("<h3> JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM </h3>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("<center>");
